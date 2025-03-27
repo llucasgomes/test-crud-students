8d2f8b738b2a07eaca8555d5c4305b5cb1c43c1a
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-27 12:32:06 -0400

    :sparkles: feat: add Dialog component

--- front-students/src/app/components/button/button.component.ts
+++ front-students/src/app/components/button/button.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogContentComponent } from "../dialog/dialog-content.component";
 
 @Component({
-  selector: 'app-button-new-student',
-  imports: [RouterModule],
-  standalone: true,
+  selector: 'app-new-student-dialog-opener',
+  imports: [MatButtonModule, MatDialogModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-  @Input() link = '/aluno';
+ 
+
+  readonly dialog = inject(MatDialog);  // Injeção do MatDialog
+
+  // Método para abrir o diálogo
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogContentComponent);  // Abre o componente DialogContentComponent
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Resultado do diálogo:', result);  // Exibe o valor após o fechamento do diálogo
+    });
+  }
 }
+
+
+
+
