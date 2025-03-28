4a256a0cc5b78fc28f73695ee6de43d76f418268
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-27 23:47:22 -0400

    :sparkles: feat: add 'getStudentByIdService'

--- back-node-students/src/controllers/student.controller.ts
+++ back-node-students/src/controllers/student.controller.ts
@@ -3,12 +3,14 @@ import {
   createStudentService,
   deleteStudentByIdService,
   getAllStudentService,
+  getStudentByIdService,
   updateStudentService,
 } from '../services/student.service'
 
 export default async function studentController(server: FastifyInstance) {
   server.post('/', createStudentService),
     server.get('/', getAllStudentService),
+    server.get('/:id', getStudentByIdService),
     server.put('/:id', updateStudentService),
     server.delete('/:id', deleteStudentByIdService)
 }
