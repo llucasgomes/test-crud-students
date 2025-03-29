5e147112f3421135d984eba5ba09df2b18399849
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-29 09:05:23 -0400

    :sparkles: feat: created model student

--- back-aspnet-students/obj/Debug/net8.0/back-aspnet-students.AssemblyInfo.cs
+++ back-aspnet-students/obj/Debug/net8.0/back-aspnet-students.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-aspnet-students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f908c6996e00dbf82f7f7e2b943d91bf21c0915")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97147d9b36f4754a35e5555c34ac8143146c3db")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-aspnet-students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-aspnet-students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
