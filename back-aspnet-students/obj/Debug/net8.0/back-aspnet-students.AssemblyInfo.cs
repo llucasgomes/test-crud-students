830a4860d62ee27c42268329c97eff8c4ada1562
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-29 08:36:12 -0400

    :heavy_plus_sign: build: add  PostgreSQL e Entity Framework Core

--- back-aspnet-students/obj/Debug/net8.0/back-aspnet-students.AssemblyInfo.cs
+++ back-aspnet-students/obj/Debug/net8.0/back-aspnet-students.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-aspnet-students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891fb09a5fc0b33e3a9c9eeb367eede1fa7fded5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f908c6996e00dbf82f7f7e2b943d91bf21c0915")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-aspnet-students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-aspnet-students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
