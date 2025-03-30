5101fc749f820b505c7efe8e23cc0476acd5c2b9
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-29 20:03:52 -0400

    :sparkles: feat: created student model

---
+++ back-aspnet-students/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace back_aspnet_students.Models;
+
+public class Student
+{
+   public string Id { get; set; } = string.Empty; 
+    public string Name { get; set; } = string.Empty; 
+    public string Email { get; set; } = string.Empty;
+    public string Course { get; set; } = string.Empty; 
+    public DateTime CreatedAt { get; set; } 
+    public DateTime UpdatedAt { get; set; }  
+}
