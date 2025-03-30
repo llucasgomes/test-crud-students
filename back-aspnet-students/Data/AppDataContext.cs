6f5092edc4c17c40e7c933dbaca8e5d66b7d857c
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-29 20:09:46 -0400

    :sparkles: feat: created context database

---
+++ back-aspnet-students/Data/AppDataContext.cs
@@ -0,0 +1,11 @@
+using back_aspnet_students.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_aspnet_students.Data;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options): base(options){}
+    public DbSet<Student> Students {get;set;} = null!;
+    
+}
