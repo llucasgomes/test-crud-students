74d3932e2fda4434f8e08b2b7220b498992df892
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-30 23:26:33 -0400

    :bug: fix: CORS error in API

--- back-aspnet-students/Program.cs
+++ back-aspnet-students/Program.cs
@@ -16,7 +16,20 @@ options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")
 // Adicionando suporte a Controllers
 builder.Services.AddControllers();
 
+// Adiciona o serviço CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() // Permite qualquer origem
+              .AllowAnyHeader()  // Permite qualquer cabeçalho
+              .AllowAnyMethod(); // Permite qualquer método HTTP
+    });
+});
+
 var app = builder.Build();
+// Adiciona a política CORS para toda a aplicação
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,29 +44,10 @@ app.UseAuthorization(); // Adicionando autorização
 
 app.MapControllers(); // Adicionando mapeamento para Controllers
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
