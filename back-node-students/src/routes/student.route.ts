0d066624667032cec46c6a9b35871c29a2a6de2c
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-30 20:26:10 -0400

    :wrench: chore: changed route prefix to 'students'

--- back-node-students/src/routes/student.route.ts
+++ back-node-students/src/routes/student.route.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from 'fastify'
 import studentController from '../controllers/student.controller'
 
 export default async function studentRoutes(server: FastifyInstance) {
-  server.register(studentController, { prefix: '/student' })
+  server.register(studentController, { prefix: '/students' })
 }
